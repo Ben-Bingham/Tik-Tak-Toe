0db4970734b5d593d445f7c2f5ac31e32afb212c
Ben-Bingham <94578530+Ben-Bingham@users.noreply.github.com>
2022-10-05 09:26:47 -0600

    Single player done

--- script1.js
+++ script1.js
@@ -5,7 +5,23 @@ const board = [0, 0, 0,
 var playersTurn = 1;
 var movesMade = 0;
 
-function buttonClick(index) {
+function botMove() {
+    var moveNotMade = true;
+    while(moveNotMade) {
+        var randomNumber = Math.floor(Math.random() * 9);
+        if (board[randomNumber] == 0) {
+            buttonClick(randomNumber, true);
+            moveNotMade = false;
+        }
+    }
+}
+
+function buttonClick(index, bot) {
+    if (index >= 9) {
+        alert("Index out of range.");
+        return;
+    }
+
     if (playersTurn == 1) {
         document.getElementById(index.toString()).innerHTML = "<div class=\"ButtonDiv\"><button class=\"clickedButton Button\">X</button></div>";
     }
@@ -13,14 +29,11 @@ function buttonClick(index) {
         document.getElementById(index.toString()).innerHTML = "<div class=\"ButtonDiv\"><button class=\"clickedButton Button\">O</button></div>";
     }
 
-    if (index >= 9) {
-        alert("Index out of range.");
-        return;
-    }
-
     board[index] = playersTurn;
+    console.log(playersTurn);
 
-    detectWin();
+    var win = detectWin();
+    console.log(playersTurn);
 
     if (playersTurn == 1) {
         playersTurn = 2;
@@ -28,16 +41,11 @@ function buttonClick(index) {
     else {
         playersTurn = 1;
     }
+    console.log(playersTurn);
 
     movesMade++;
-
-    var moveNotMade = true;
-    while(moveNotMade) {
-        var randomNumber = Math.floor(Math.random() * 9);
-        if (board[randomNumber] == 0) {
-            moveNotMade = false;
-            board[randomNumber] = playersTurn;
-        }
+    if (!bot && !win) {
+        botMove();
     }
 }
 
@@ -52,12 +60,22 @@ function playAgain() {
     movesMade = 0;
 }
 
-function anounceWin() {
-    setTimeout(alertWin, 1);
+function anouncePlayerWin() {
+    setTimeout(alertPlayerWin, 1);
 }
 
-function alertWin() {
-    if(confirm("Player " + playersTurn.toString() + " Wins!\nDo you want to play again?")) {
+function anounceBotWin() {
+    setTimeout(alertBotWin, 1);
+}
+
+function alertPlayerWin() {
+    if(confirm("The Player Wins!\nDo you want to play again?")) {
+        playAgain();
+    }
+}
+
+function alertBotWin() {
+    if(confirm("The AI Wins!\nDo you want to play again?")) {
         playAgain();
     }
 }
@@ -73,42 +91,85 @@ function alertDraw() {
 }
 
 function detectWin() {
+    //console.log(playersTurn);
+
     if (board[0] == board[1] && board[1] == board[2] && board[0] != 0) {
-        anounceWin();
-        return;
+        if (board[0] == 1) {
+            anouncePlayerWin();
+        }
+        else {
+            anounceBotWin();
+        }
+        return true;
     }
     if (board[3] == board[4] && board[4] == board[5] && board[3] != 0) {
-        anounceWin();
-        return;
+        if (board[3] == 1) {
+            anouncePlayerWin();
+        }
+        else {
+            anounceBotWin();
+        }        
+        return true;
     }
     if (board[6] == board[7] && board[7] == board[8] && board[6] != 0) {
-        anounceWin();
-        return;
+        if (board[6] == 1) {
+            anouncePlayerWin();
+        }
+        else {
+            anounceBotWin();
+        }        
+        return true;
     }
 
     if (board[0] == board[3] && board[3] == board[6] && board[0] != 0) {
-        anounceWin();
-        return;
+        if (board[0] == 1) {
+            anouncePlayerWin();
+        }
+        else {
+            anounceBotWin();
+        }        
+        return true;
     }
     if (board[1] == board[4] && board[4] == board[7] && board[1] != 0) {
-        anounceWin();
-        return;
+        if (board[1] == 1) {
+            anouncePlayerWin();
+        }
+        else {
+            anounceBotWin();
+        }        
+        return true;
     }
     if (board[2] == board[5] && board[5] == board[8] && board[2] != 0) {
-        anounceWin();
-        return;
+        if (board[2] == 1) {
+            anouncePlayerWin();
+        }
+        else {
+            anounceBotWin();
+        }
+        return true;
     }
 
     if (board[0] == board[4] && board[4] == board[8] && board[0] != 0) {
-        anounceWin();
-        return;
+        if (board[0] == 1) {
+            anouncePlayerWin();
+        }
+        else {
+            anounceBotWin();
+        }
+        return true;
     }
     if (board[6] == board[4] && board[4] == board[2] && board[6] != 0) {
-        anounceWin();
-        return;
+        if (board[6] == 1) {
+            anouncePlayerWin();
+        }
+        else {
+            anounceBotWin();
+        }
+        return true;
     }
 
     if (movesMade == 8) {
         anounceDraw();
+        return true;
     }
 }
